d8bd03f0bfa5e93c8c0aa8b431ada84ec1a44967
Joe <56852050+STLemons@users.noreply.github.com>
2025-09-09 13:58:52 +0800

    main.dart

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'DHLink',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -27,4 +28,5 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
     );
   }
+
 }
